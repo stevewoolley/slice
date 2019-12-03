adee1ab47ce8272ef9401577d1afdbe430701078
Steve Woolley <steve.woolley@genesys.com>
2019-12-03 13:17:43 -0500

    first cut at inputPub. add datetime to motion publish

--- inputPub.py
+++ inputPub.py
@@ -1,30 +1,29 @@
 #!/usr/bin/env python
 
-from iot import AllowedActions, LOG_FORMAT
-import platform
-import argparse
-from signal import pause
-from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
-from gpiozero import MotionSensor
-import logging
 import json
-import datetime
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+from signal import pause
+from gpiozero import Button
+import argparse
+import platform
+from iot import iot_thing_topic, iot_payload, AllowedActions
 
 
-def motion():
-    logging.info('Motion')
-    try:
-        myAWSIoTMQTTClient.connect()
-        myAWSIoTMQTTClient.publish(args.topic, json.dumps(
-            {'thing': args.thingName,
-             'observed': datetime.datetime.now().isoformat()}), 0)
-        myAWSIoTMQTTClient.disconnect()
-    except Exception as err:
-        logging.warning('{}'.format(err))
+def publish(topic, value):
+    myAWSIoTMQTTClient.publish(
+        topic,
+        json.dumps({args.shadow_var: value, 'message': "{} {}".format(args.shadow_var, value)}), 1)
+    myAWSIoTMQTTClient.publish(
+        iot_thing_topic(args.thing),
+        iot_payload('reported', {args.shadow_var: value}), 1)
 
 
-def no_motion():
-    logging.info('No Motion')
+def high():
+    publish(args.topic, args.high_value)
+
+
+def low():
+    publish(args.low_topic, args.low_value)
 
 
 if __name__ == "__main__":
@@ -46,18 +45,22 @@ if __name__ == "__main__":
     parser.add_argument("-n", "--thingName", action="store", dest="thingName", default=platform.node().split('.')[0],
                         help="Targeted thing name")
     parser.add_argument("--pin", action="store", dest="pin", help="gpio pin (using BCM numbering)", type=int)
-    parser.add_argument("-q", "--queue_len",
-                        help="The length of the queue used to store values read from the sensor. (1 = disabled)",
-                        type=int, default=1)
-    parser.add_argument("--sample_rate",
-                        help="The number of values to read from the device " +
-                             "(and append to the internal queue) per second",
-                        type=float, default=100)
-    parser.add_argument("-x", "--threshold",
-                        help="When the mean of all values in the internal queue rises above this value, " +
-                             "the sensor will be considered active by the is_active property, " +
-                             "and all appropriate events will be fired",
-                        type=float, default=0.5)
+    parser.add_argument("-u", "--pull_up",
+                        help="If True (the default), the GPIO pin will be pulled high by default. " +
+                             "In this case, connect the other side of the button to ground. " +
+                             "If False, the GPIO pin will be pulled low by default. " +
+                             "In this case, connect the other side of the button to 3V3",
+                        default=True)
+    parser.add_argument("-b", "--bounce_time",
+                        help="If None (the default), no software bounce compensation will be performed. " +
+                             "Otherwise, this is the length of time (in seconds) " +
+                             "that the component will ignore changes in state after an initial change.",
+                        type=float, default=None)
+    parser.add_argument("-s", "--shadow_var", help="Shadow variable", required=True)
+    parser.add_argument("-y", "--high_value", help="high value", default=1)
+    parser.add_argument("-z", "--low_value", help="low value", default=0)
+    parser.add_argument("-o", "--low_topic", action="store", dest="low_topic",
+                        help="Low topic (defaults to topic if not assigned")
     args = parser.parse_args()
 
     if args.mode not in AllowedActions:
@@ -79,9 +82,6 @@ if __name__ == "__main__":
     if not args.useWebsocket and not args.port:  # When no port override for non-WebSocket, default to 8883
         port = 8883
 
-    # Configure logging
-    logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
-
     # Init AWSIoTMQTTClient
     myAWSIoTMQTTClient = None
     if args.useWebsocket:
@@ -100,9 +100,16 @@ if __name__ == "__main__":
     myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
     myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
 
-    pir = MotionSensor(args.pin, queue_len=args.queue_len, sample_rate=args.sample_rate, threshold=args.threshold)
+    # Connect and subscribe to AWS IoT
+    myAWSIoTMQTTClient.connect()
 
-    pir.when_motion = motion
-    pir.when_no_motion = no_motion
+    inp = Button(args.pin, pull_up=args.pull_up, bounce_time=args.bounce_time)
+
+    # default low_topic to topic if not defined
+    if args.low_topic is None or len(args.low_topic) == 0:
+        args.low_topic = args.topic
+
+    inp.when_pressed = high
+    inp.when_released = low
 
     pause()
